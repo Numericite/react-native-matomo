22d23ce722f526113dc6b8b9bba200209be8d581
Clément <clement@numericite.eu>
2022-07-07 11:00:29 +0200

    feat: init

---
+++ android/build/generated/source/buildConfig/debug/de/bonify/reactnativematomo/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package de.bonify.reactnativematomo;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "de.bonify.reactnativematomo";
+  public static final String BUILD_TYPE = "debug";
+}
